c3eea302423f337d3750f463645a081427e77f00
redhair <thomasbonanni@afito.com>
2020-02-19 23:53:48 -0500

    Added webpack, babel, husky, eslint, travis, ignore files, and module skeleton.

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  externals: {},
+  resolve: {
+    extensions: ['.scss', '.js', '.json', '.png', '.gif', '.jpg', '.svg']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(png|gif|jpg|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    filename: 'image-sizer.js',
+    libraryTarget: 'commonjs2'
+  },
+  optimization: {
+    minimize: false
+  }
+};
